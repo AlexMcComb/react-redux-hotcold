2012d2ae345b21b8bb55c153ce43699809f1e6c1
Alex <alex@Alexs-MacBook-Pro.local>
2019-01-24 16:27:31 -0700

    add solution

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,18 @@
 //Anything that originally used "setState" in the component is now being turned into an action here
 
-export const MAKE_GUESS = "MAKE_GUESS";
-export const makeGuess = guess => ({
-  type: MAKE_GUESS,
-  guess,
-  feedback,
-  correctAnswer
+export const GENERATE_AURAL_UPDATE = "GENERATE_AURAL_UPDATE";
+export const generateAuralUpdate = () => ({
+  type: GENERATE_AURAL_UPDATE
 });
 
 export const RESTART_GAME = "RESTART_GAME";
-export const restartGame = () => ({
+export const restartGame = correctAnswer => ({
   type: RESTART_GAME,
-  guess,
-  feedback,
   correctAnswer
+});
+
+export const MAKE_GUESS = "MAKE_GUESS";
+export const makeGuess = guess => ({
+  type: MAKE_GUESS,
+  guess
 });
--- src/components/game.js
+++ src/components/game.js
@@ -4,91 +4,16 @@ import Header from "./header";
 import GuessSection from "./guess-section";
 import StatusSection from "./status-section";
 import InfoSection from "./info-section";
-import { makeGuess, restartGame } from "../actions";
 
-export class Game extends React.Component {
-  restartGame() {
-    this.props.dispatch(restartGame());
-  }
-
-  makeGuess(guess) {
-    guess = parseInt(guess, 10);
-    if (isNaN(guess)) {
-      this.setState({ feedback: "Please enter a valid number" });
-    }
-
-    const difference = Math.abs(guess - this.state.correctAnswer);
-
-    let feedback;
-    if (difference >= 50) {
-      feedback = "You're Ice Cold...";
-    } else if (difference >= 30) {
-      feedback = "You're Cold...";
-    } else if (difference >= 10) {
-      feedback = "You're Warm.";
-    } else if (difference >= 1) {
-      feedback = "You're Hot!";
-    } else {
-      feedback = "You got it!";
-    }
-
-    this.props.dispatch(makeGuess(guess));
-
-    // We typically wouldn't touch the DOM directly like this in React
-    // but this is the best way to update the title of the page,
-    // which is good for giving screen-reader users
-    // instant information about the app.
-    document.title = feedback ? `${feedback} | Hot or Cold` : "Hot or Cold";
-  }
-
-  generateAuralUpdate() {
-    const { guesses, feedback } = this.state;
-
-    // If there's not exactly 1 guess, we want to
-    // pluralize the nouns in this aural update.
-    const pluralize = guesses.length !== 1;
-
-    let auralStatus = `Here's the status of the game right now: ${feedback} You've made ${
-      guesses.length
-    } ${pluralize ? "guesses" : "guess"}.`;
-
-    if (guesses.length > 0) {
-      auralStatus += ` ${
-        pluralize ? "In order of most- to least-recent, they are" : "It was"
-      }: ${guesses.reverse().join(", ")}`;
-    }
-
-    this.setState({ auralStatus });
-  }
-
-  render() {
-    const { feedback, guesses, auralStatus } = this.state;
-    const guessCount = guesses.length;
-
-    return (
-      <div>
-        <Header
-          onRestartGame={() => this.restartGame()}
-          onGenerateAuralUpdate={() => this.generateAuralUpdate()}
-        />
-        <main role="main">
-          <GuessSection
-            feedback={feedback}
-            guessCount={guessCount}
-            onMakeGuess={guess => this.makeGuess(guess)}
-          />
-          <StatusSection guesses={guesses} auralStatus={auralStatus} />
-          <InfoSection />
-        </main>
-      </div>
-    );
-  }
+export default function Game(props) {
+  return (
+    <div>
+      <Header />
+      <main role="main">
+        <GuessSection />
+        <StatusSection />
+        <InfoSection />
+      </main>
+    </div>
+  );
 }
-
-export const mapStateToProps = state => ({
-  guess: state.guess,
-  feedback: state.feedback,
-  correctAnswer: Math.floor(Math.random() * 100) + 1
-});
-
-export default connect(mapStateToProps)(Game);
